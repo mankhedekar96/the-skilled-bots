2ee204fffb43db56c929b80007a3dc3313378d14
Manish Khedekar <manish@payroo.com.au>
2020-11-19 14:50:05 +0530

    Added responsive footer

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll',function(){
     let navOptions = document.querySelectorAll('#navigator a');
-    navOptions.forEach(el => el.setAttribute('class',''))
+    navOptions.length && navOptions.forEach(el => el.setAttribute('class',''))
     let navOption = document.getElementById('navOption' + (parseInt(window.pageYOffset/900) + 1));
-    navOption.setAttribute('class','active');
+    navOption && navOption.setAttribute('class','active');
 });
